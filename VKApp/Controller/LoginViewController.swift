e3cc7ab0891e726a93101efd454b2f251db38769
pkslty <pkslty@yandex.ru>
2021-04-02 16:49:33 +0300

    Добавлено скрытие клавиатуры, кнопка для показа пароля, главный вьюконтроллер и переход на него по нажатию кнопки логин

--- VKApp/Controller/LoginViewController.swift
+++ VKApp/Controller/LoginViewController.swift
@@ -11,16 +11,61 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginScrollView: UIScrollView!
     @IBOutlet weak var loginContentView: UIView!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    var eyeButton: UIButton?
+        
+    @IBAction func loginPressed(_ sender: Any) {
+        
+
+    }
+    
+    @objc func hideKeyboerd() {
+        loginScrollView.endEditing(true)
+    }
+    
+    @objc func showHidePassword() {
+        if password.isSecureTextEntry {
+            password.isSecureTextEntry = false
+            eyeButton?.tintColor = .systemBlue
+        } else {
+            password.isSecureTextEntry = true
+            eyeButton?.tintColor = .green
+        }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+ 
+        guard let username = username.text,
+              let password = password.text,
+              username == "dnk",
+              password == "12345"
+        else {
+            return false
+        }
+            
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        //let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 1000.0, right: 0.0)
-        //
-        //loginScrollView.contentInset = contentInsets
-        //loginScrollView.scrollIndicatorInsets = contentInsets
+        let keyboardHideGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboerd))
+        loginScrollView.addGestureRecognizer(keyboardHideGesture)
+        
+        //Add eye button to show password
+        let eyeButtonRect = CGRect(x: 0, y: 0, width: password.frame.height, height: password.frame.height)
+        eyeButton = UIButton(frame: eyeButtonRect)
+        eyeButton?.tintColor = .green
+        eyeButton?.setImage(UIImage(systemName: "eye"), for: .normal)
+        password.rightView = eyeButton
+        password.rightViewMode = UITextField.ViewMode.always
+        //Add gesture recognizer for it
         
         
+        
+        let eyeTapGesture = UITapGestureRecognizer(target: self, action: #selector(showHidePassword))
+        eyeButton?.addGestureRecognizer(eyeTapGesture)
 
     }
     
@@ -33,6 +78,12 @@ class LoginViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         }
     
+    override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
     @objc func keyboardWasShown(notification: Notification) {
             
     // Получаем размер клавиатуры
